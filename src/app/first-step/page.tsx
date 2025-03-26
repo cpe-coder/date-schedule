1f1e9fd6415ce3afe4bc18c64d6cd703bb430448
Grian Gajila <griangajila2@gmail.com>
2025-03-26 18:24:35 +0800

    wip

---
+++ src/app/first-step/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { ArrowRight } from "lucide-react";
+import React from "react";
+
+export default function FirstStep() {
+	const [firstName, setFistName] = React.useState("");
+	const [visible, setVisible] = React.useState(false);
+
+	const name = "joana";
+
+	React.useEffect(() => {
+		if (firstName == "") {
+			setVisible(true);
+		} else {
+			setVisible(false);
+		}
+	}, [firstName]);
+
+	const handleClick = () => {
+		if (firstName != name.toLowerCase()) {
+		}
+	};
+
+	return (
+		<div className="flex flex-col items-center bg-pink-300 justify-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+			<div className="bg-pink-500 p-10 rounded-2xl border-pink-700 border-2 shadow-2xl shadow-pink-700 gap-5 flex-col flex">
+				<h1 className="text-xl font-bold text-wrap max-w-4xl">
+					Before you proceed, please enter your first name.
+				</h1>
+				<input
+					type="text"
+					name="firstName"
+					id=""
+					value={firstName}
+					onChange={(e) => setFistName(e.target.value)}
+					className="outline-0 ring-0 placeholder:text-gray-200 focus:border-b-2 focus:border-b-pink-300 py-2"
+					placeholder="Type your first name here:"
+				/>
+			</div>
+
+			<AnimatePresence initial={false}>
+				<div>
+					{!visible ? (
+						<motion.button
+							initial={{ opacity: 0, scale: 0 }}
+							animate={{ opacity: 1, scale: 1 }}
+							exit={{ opacity: 0, y: -2 }}
+							whileTap={{ y: 2 }}
+							key="box"
+							onClick={handleClick}
+							className="bg-pink-500 px-6 py-2 flex gap-7 items-center rounded-xl hover:cursor-pointer hover:bg-pink-600 active:opacity-40 transition-all duration-300 active:transition-all hover:transition-all active:duration-300 hover:duration-300"
+						>
+							<span>Next</span>
+							<ArrowRight color="white" size={20} />
+						</motion.button>
+					) : null}
+				</div>
+			</AnimatePresence>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,78 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
-import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
+import Heart from "../app/assets/icon/heart.png";
 
 export default function Home() {
-	const [firstName, setFistName] = React.useState("");
-	const [visible, setVisible] = React.useState(false);
+	const [count, setCount] = React.useState(0);
+	const intervalRef = React.useRef<NodeJS.Timeout | null>(null);
+	const [loading, setLoading] = React.useState(false);
+	const route = useRouter();
 
-	const name = "joana";
-
-	React.useEffect(() => {
-		if (firstName == "") {
-			setVisible(true);
-		} else {
-			setVisible(false);
-		}
-	}, [firstName]);
-
-	const handleClick = () => {
-		if (firstName != name.toLowerCase()) {
+	const startCounting = () => {
+		if (!intervalRef.current) {
+			intervalRef.current = setInterval(() => {
+				setCount((prev) => {
+					if (prev < 10) {
+						return prev + 1;
+					} else {
+						clearInterval(intervalRef.current!);
+						intervalRef.current = null;
+						return prev;
+					}
+				});
+			}, 500);
 		}
 	};
 
+	const value = count * 10;
+	const size = value + 100;
+
+	React.useEffect(() => {
+		if (count === 10) {
+			setLoading(true);
+			setTimeout(() => {
+				route.push("/first-step");
+			}, 3000);
+		}
+	}, [count, route]);
+
+	const stopCounting = () => {
+		if (intervalRef.current) {
+			clearInterval(intervalRef.current);
+			intervalRef.current = null;
+		}
+	};
 	return (
 		<div className="flex flex-col items-center bg-pink-300 justify-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<div className="bg-pink-500 p-10 rounded-2xl border-pink-700 border-2 shadow-2xl shadow-pink-700 gap-5 flex-col flex">
-				<h1 className="text-xl font-bold text-wrap max-w-4xl">
-					Before you proceed, please enter your first name.
-				</h1>
-				<input
-					type="text"
-					name="firstName"
-					id=""
-					value={firstName}
-					onChange={(e) => setFistName(e.target.value)}
-					className="outline-0 ring-0 placeholder:text-gray-200 focus:border-b-2 focus:border-b-pink-300 py-2"
-					placeholder="Type your first name here:"
-				/>
+			<h1 className="font-bold text-pink-500">Hold me to continue!</h1>
+			<div className="rounded-full w-[200px] h-[200px] border-2 border-pink-500 shadow-2xl shadow-pink-500 flex items-center justify-center">
+				<button
+					onMouseDown={startCounting}
+					onMouseUp={stopCounting}
+					onMouseLeave={stopCounting}
+					onTouchStart={startCounting}
+					onTouchEnd={stopCounting}
+					style={{
+						width: `${size}px`,
+						height: `${size}px`,
+						fontSize: `${size / 10}px`,
+					}}
+					className="p-4 bg-pink-500 rounded-full flex items-center justify-center shadow-2xl hover:cursor-pointer text-white"
+				>
+					{loading ? (
+						<Image
+							alt="heart"
+							src={Heart}
+							className="w-28 h-28 animate-pulse"
+						/>
+					) : (
+						<span>Hold Me</span>
+					)}
+				</button>
 			</div>
-
-			<AnimatePresence initial={false}>
-				<div>
-					{!visible ? (
-						<motion.button
-							initial={{ opacity: 0, scale: 0 }}
-							animate={{ opacity: 1, scale: 1 }}
-							exit={{ opacity: 0, y: -2 }}
-							whileTap={{ y: 2 }}
-							key="box"
-							onClick={handleClick}
-							className="bg-pink-500 px-6 py-2 flex gap-7 items-center rounded-xl hover:cursor-pointer hover:bg-pink-600 active:opacity-40 transition-all duration-300 active:transition-all hover:transition-all active:duration-300 hover:duration-300"
-						>
-							<span>Next</span>
-							<ArrowRight color="white" size={20} />
-						</motion.button>
-					) : null}
-				</div>
-			</AnimatePresence>
 		</div>
 	);
 }
